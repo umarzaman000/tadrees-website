7997ec290ed9d7f13fe33787f04422beee912a64
Umar's <umarzaman124@gmail.com>
2024-04-16 16:13:30 +0500

    all

--- teacher.js
+++ teacher.js
@@ -5,7 +5,7 @@ var teacher = [
     designation: "CEO tadrees, Co-Founder tadrees & Eastman Innovations",
     linkedin: "https://www.linkedin.com/in/khan-muhammad-riaz-460918180/",
     experiance: "10 years",
-    education: "ask",
+    education: "BS(CS)",
   },
   {
     image: "/assets/img/ehsanbhai.jpeg",
@@ -13,7 +13,7 @@ var teacher = [
     designation: "Co-Founder/CTO @ Eastman Innovations",
     linkedin: "https://www.linkedin.com/in/sayyed-muhammad-ehsan/",
     experiance: "10 years",
-    education: "ask",
+    education: "BS(CS)",
   },
   {
     image: "/assets/img/asimbhai1.jpeg",
@@ -22,7 +22,7 @@ var teacher = [
     linkedin:
       "https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAAC3_qd0B-alpOlM9J1P0XTtbT5D20LUruB0&keywords=asim%20waqar&origin=RICH_QUERY_TYPEAHEAD_HISTORY&position=0&searchId=9c4055a7-47b1-4af2-8004-f31790d0b0ec&sid=9Nt&spellCorrectionEnabled=true",
     experiance: "10 years",
-    education: "ask",
+    education: "BS(CS)",
   },
   {
     image: "/assets/img/ahsanbhai.jpeg",
@@ -31,7 +31,7 @@ var teacher = [
     linkedin:
       "https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAACJjdT4BjCRGRvB5VVCo9a__tAax-O0GBV0&keywords=ahsan%20munir&origin=RICH_QUERY_TYPEAHEAD_HISTORY&position=0&searchId=0b65d505-33ff-4956-baf6-a0e21eda8ef2&sid=%40_V&spellCorrectionEnabled=true",
     experiance: "10 years",
-    education: "ask",
+    education: "BS(CS)",
   },
   {
     image: "/assets/img/khizarbhai1.jpeg",
--- testimonial.js
+++ testimonial.js
@@ -8,10 +8,16 @@ var testimonial = [
     ],
     name: "Furqan Rauf",
     image: "/assets/img/furaqan.jpeg",
+    image1: "/assets/img/javascript.webp",
+    image2: "/assets/img/css.png",
+    image3: "/assets/img/newimagebasic.jpeg",
+    image4: "/assets/img/reactjs.png",
     Designation: "Senior Software Engineer at Cloudpacer",
+    content:
+      "Now, as I navigate my career in a prominent software company, I am continually grateful for the solid foundation and continuous support provided by [Institute Name]. Their commitment to fostering talent and nurturing future industry leaders is evident in every aspect of their program. I am proud to be a product of their exceptional training and would highly recommend [Institute Name] to anyone aspiring to excel in the field of software development.",
     Designationicon: "fas fa-laptop",
-    linkedin:"https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADRtVrkBwOA1kHgqjTvRRYU0uOGBBsezCM0&keywords=furqan%20rauf&origin=RICH_QUERY_TYPEAHEAD_HISTORY&position=0&searchId=b328638d-62e3-4e4a-bbd7-5ffa359ae108&sid=O(O&spellCorrectionEnabled=true",
-
+    linkedin:
+      "https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADRtVrkBwOA1kHgqjTvRRYU0uOGBBsezCM0&keywords=furqan%20rauf&origin=RICH_QUERY_TYPEAHEAD_HISTORY&position=0&searchId=b328638d-62e3-4e4a-bbd7-5ffa359ae108&sid=O(O&spellCorrectionEnabled=true",
   },
   {
     courses: [
@@ -24,8 +30,7 @@ var testimonial = [
     image: "/assets/img/qasim.jpeg",
     Designation: "Software Engineer at Eastman Innovations",
     Designationicon: "fas fa-laptop",
-    linkedin:"https://www.linkedin.com/in/qasim-hashmi-23a5b82b8/"
-    
+    linkedin: "https://www.linkedin.com/in/qasim-hashmi-23a5b82b8/",
   },
   {
     courses: [
@@ -38,6 +43,7 @@ var testimonial = [
     image: "/assets/img/alibhaimalik.jpeg",
     Designation: "Senior Software Engineer at Eastman Inovations",
     Designationicon: "fas fa-laptop",
-    linkedin:"https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADWe-_YBzMO_cnDRRc12mOtBIOK7AwRaLjg&keywords=ali%20malik&origin=RICH_QUERY_SUGGESTION&position=1&searchId=9a7ad83c-a64e-43d4-865d-216d6ae24521&sid=Iv5&spellCorrectionEnabled=false"
+    linkedin:
+      "https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADWe-_YBzMO_cnDRRc12mOtBIOK7AwRaLjg&keywords=ali%20malik&origin=RICH_QUERY_SUGGESTION&position=1&searchId=9a7ad83c-a64e-43d4-865d-216d6ae24521&sid=Iv5&spellCorrectionEnabled=false",
   },
 ];
