14849d346d90688556db7e598c89b76b92cc2bfa
Umar's <umarzaman124@gmail.com>
2024-04-17 14:28:14 +0500

    all

--- phone.js
+++ phone.js
@@ -2,6 +2,6 @@ var contactdetail = [
   {
     phonenumber: "+92 331 4488908",
     address: "Raiwind Road, Lahore",
-    workinghours: "Mon - Fri 8 AM - 5 PM",
+    workinghours: "Mon - Fri 9 AM - 9 PM",
   },
 ];
