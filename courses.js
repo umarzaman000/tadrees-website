0920b6c4d92793101756fd1f3f88608c6be5fd28
Umar's <umarzaman124@gmail.com>
2024-04-17 16:22:35 +0500

    all

--- courses.js
+++ courses.js
@@ -20,12 +20,12 @@ var courses = [
     description: "Frontend Engineering is the art of crafting immersive and user-centric experiences on the web, shaping the visual and interactive aspects of websites and applications. Frontend Engineers play a pivotal role in bringing designs to life and ensuring seamless interaction between users and digital platforms.",
     prerequsite: "programing fundamentals.",
     whatsappMessage: "Hi I Want To Enroll In Intro To Web Development.",
-    duration: "3 Months.",
+    duration: "3 Months",
     price: "RS. 21,000/Month",
-    programoutline:"42 Lectures, 10 Assignments, 1 Project, 3Quizzes, Mid-term Evalution, Final Evalution.",
+    programoutline:"42 Lectures, 10 Assignments, 1 Project, 3 Quizzes, Mid-term Evalution, Final Evalution.",
     classhours:"10 AM - 12 AM",
     studentage:"16 Years",
-    classsize:"15 kidz"
+    classsize:"15 Students"
   },
   {
     learnings: [
@@ -47,13 +47,13 @@ var courses = [
     image4:"/assets/img/reactjs.png",
     description: "With our expertise in Frontend Engineering, we specialize in transforming concepts into interactive realities. Whether it's designing sleek and responsive layouts, optimizing performance for seamless user interaction, or ensuring cross-browser compatibility for a consistent experience across devices, our team is dedicated to delivering excellence at every step.",
     whatsappMessage: "Hi I Want To Enroll In Frontend Engineering.",
-    duration: "6 Months.",
+    duration: "6 Months",
     prerequsite: "intro to web development.",
     price: "RS. 21,000/Month",
-    programoutline:"84 Lectures, 10 Assignments, 1 Project, 3Quizzes, Mid-term Evalution, Final Evalution.",
+    programoutline:"84 Lectures, 10 Assignments, 1 Project, 3 Quizzes, Mid-term Evalution, Final Evalution.",
     classhours:"10 AM - 12 AM",
     studentage:"16 Years",
-    classsize:"15 kidz"
+    classsize:"15 Students"
   },
   {
     learnings: [
@@ -75,13 +75,13 @@ var courses = [
     image4:"/assets/img/codeimage.jpeg",
     description: "In our Backend Engineering course, we dive deep into the intricacies of server-side development, equipping learners with the skills and knowledge needed to design, build, and maintain powerful backend systems. From setting up server environments and managing databases to implementing APIs and handling authentication, our curriculum covers all aspects of backend development.",
     whatsappMessage: "Hi I Want To Enroll In Backend Engineering.",
-    duration: "6 Months.",
+    duration: "6 Months",
     prerequsite: "intro to web development.",
     price: "RS. 21,000/Month",
-    programoutline:"84 Lectures, 10 Assignments, 1 Project, 3Quizzes, Mid-term Evalution, Final Evalution.",
+    programoutline:"84 Lectures, 10 Assignments, 1 Project, 3 Quizzes, Mid-term Evalution, Final Evalution.",
     classhours:"10 AM - 12 AM",
     studentage:"16 Years",
-    classsize:"15 kidz",
+    classsize:"15 Students",
   },
   {
     learnings: [
@@ -103,12 +103,12 @@ var courses = [
     image4:"/assets/img/colorsideas.png",
     description: "Whether you're a beginner looking to kickstart your journey into the world of coding or an experienced developer seeking to solidify your understanding of programming fundamentals, our course provides a comprehensive and accessible learning experience tailored to your needs. Join us and embark on your path to becoming a proficient and confident programmer.",
     whatsappMessage: "Hi I Want To Enroll In Programing Fundamentals.",
-    duration: "3 Months.",
+    duration: "3 Months",
     prerequsite: "Admission Interview.",
     price: "RS. 21,000/Month",
-    programoutline:"42 Lectures, 10 Assignments, 1 Project, 3Quizzes, Mid-term Evalution, Final Evalution.",
+    programoutline:"42 Lectures, 10 Assignments, 1 Project, 3 Quizzes, Mid-term Evalution, Final Evalution.",
     classhours:"10 AM - 12 AM",
     studentage:"16 Years",
-    classsize:"15 kidz",
+    classsize:"15 Students",
   }
 ];
